483c718ef277e4cdf41ac9f39e4ef0ff7569702d
Jean Santos <jeansantosc@gmail.com>
2019-01-12 19:40:58 -0200

    setting state for youtube results

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,23 @@ import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 
 class App extends Component {
-
-    conTermSubmit = term => {
-        youtube.get('/search', {
+    state = { videos: [] };
+    
+    onTermSubmit = async term => {
+        const results = await youtube.get('/search', {
             params: {
                 q: term
             },
         });
+
+        this.setState({ videos: results.data.items });
     }
 
     render() {
         return (
             <div className='ui container'>
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                I have {this.state.videos.length} videos
             </div>
         );
     }
